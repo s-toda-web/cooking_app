619358f18aff530e620161fbd0ea1bb35560b530
s-toda-web <sho02270227@icloud.com>
2025-10-28 16:22:03 +0900

    route変更

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return redirect('/login');
+    return redirect()->route('recipes.index');
 });
 
 Route::get('/dashboard', function () {
